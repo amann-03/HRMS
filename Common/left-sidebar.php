43e299da296c0454180be04153db1740a5b42f7c
Guptesh-Ranjan-Sahoo <128808197+Guptesh-Ranjan-Sahoo@users.noreply.github.com>
2024-06-28 16:58:35 +0530

    Add files via upload

--- Common/left-sidebar.php
+++ Common/left-sidebar.php
@@ -1,8 +1,19 @@
+
 <ul class="list-group" id = "side" >
-	<li class="list-group-item"><a href="dashboard.php"><div>Dashboard</div></a></li>
-	<li class="list-group-item"><a href="Personal.php"><div>Personal</div></a></li>
-	<li class="list-group-item"><a href="Schedule.php"><div>Schedule</div></a></li>
-	<li class="list-group-item">Project</li>
-	<li class="list-group-item">Payroll</li>
+	 <li class="iist-group-item"><a href="dashboard.php"class="list-group-item <?php if(basename($_SERVER['PHP_SELF'])=="dashboard.php"){echo "active";}
+	  else {echo "";}?>">Dashboard</a>
+  </li>
+	<li class="iist-group-item"><a href="Personal.php"class="list-group-item <?php if(basename($_SERVER['PHP_SELF'])=="Personal.php"){echo "active";}
+	  else {echo "";}?>">Personal</a>
+  </li>
+	<li class="iist-group-item"><a href="Schedule.php"class="list-group-item <?php if(basename($_SERVER['PHP_SELF'])=="Schedule.php"){echo "active";}
+	  else {echo "";}?>">Schedule</a>
+  </li>
+	<li class="iist-group-item"><a href=""class="list-group-item <?php if(basename($_SERVER['PHP_SELF'])==""){echo "active";}
+	  else {echo "";}?>">Project</a>
+  </li>
+	<li class="iist-group-item"><a href=""class="list-group-item <?php if(basename($_SERVER['PHP_SELF'])==""){echo "active";}
+	  else {echo "";}?>">Payroll</a>
+  </li>
 	
 </ul>
--- Common/schedule_upper.php
+++ Common/schedule_upper.php
@@ -1,6 +1,16 @@
-<ul id="up_2" class ="bar">
-	<li>Overview</a></li>
-	<li>Attendance</a></li>
-	<li>Leave</li>
+<link href='https://fonts.googleapis.com/css?family=DM Sans' rel='stylesheet'>
+
+
+
+<ul id="up_2" class ="list-group">
+	<li class="iist-group-item"><a href="Schedule.php"class="list-group-item <?php if(basename($_SERVER['PHP_SELF'])=="Schedule.php"){echo "active";}
+	  else {echo "";}?>">Overview</a>
+  </li>
+	<li class="iist-group-item"><a href="attendance.php"class="list-group-item <?php if(basename($_SERVER['PHP_SELF'])=="attendance.php"){echo "active";}
+	  else {echo "";}?>">Attendance</a>
+  </li>
+	<li class="iist-group-item"><a href=""class="list-group-item <?php if(basename($_SERVER['PHP_SELF'])==""){echo "active";}
+	  else {echo "";}?>">Leaves</a>
+  </li>
 	
 </ul>
