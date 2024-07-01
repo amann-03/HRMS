a2b889dcab1d9d095016076598e3844432ae5f88
Aman Patel <122123520+amann-03@users.noreply.github.com>
2024-07-01 17:25:22 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php 
+
+session_start();
+require_once('config.php');
+
+?>
+
 <?php
 
 	include 'connection.php';
@@ -6,22 +13,37 @@
 		$user_email = $_POST['User_mail'];
 		$user_password = $_POST['User_password'];
 
-		$insert_query = "insert into login_page(username,password)values('$user_email','$user_password')";
+		$get_query = "select * from employee where login_id = '$user_email';";
+		$res = mysqli_query($conn, $get_query);
 
-		mysqli_query($conn, $insert_query);
+		if($res->num_rows > 0){
+
+			$user = mysqli_fetch_object($res);
+			$user_E = $user->login_id;
+			$pass = $user->password;
+			// $encrypted_pass = md5($user_password);
+
+			if($user_password == $pass){
+				$_SESSION['employee_id'] = $user->employee_id;
+		 		$_SESSION['login_id'] = $user_email;
+				header('location:dashboard.php');
+			}
+
+		}
+
 	}
 ?>
 
 
 
- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="login.css">
+	<link rel="stylesheet" type="text/css" href="CSS\login.css">
 	<title>Login</title>
 </head>
 
@@ -34,16 +56,16 @@
 <div class="flex-container">
 <div class="flex-child 1">
 <h1 id="heading">LOGIN</h1>
-<form action ="dashboard.php" method="post">
+<form action ="" method="post">
 <div class="block">
 <div>
 	<input type="email" name="User_mail" class="element" values="" placeholder="Username"required>
 </div>
 <div>
-	<input type="Password" name="User_password" class="element" values=""placeholder="Password"required>
+	<input type="Password" name="User_password" class="element" values=""placeholder="Password">
 </div>
-<div><button type="button" style="scale:1.3 ;"class="btn btn-outline-dark" data-toggle="modal" data-target="#myModal">Forgot password</button></div>
-<?php require_once('popup.php') ?>
+<div><button type="submit" name="forget_pass"style="scale:1.3 ;"class="btn btn-outline-dark" data-toggle="modal" data-target="#myModal">Forgot password</button></div>
+<div>
 	<input type="submit" name="form_submit" id="submit" value='Sign In'>
 </div>
 </div></form>
@@ -54,20 +76,22 @@
 
 
 </div>
+</div>
 
 
 
 </body>
 </html>
 
-<?php require_once('popup.php');
+<?php 
 
 	if(isset($_POST['forget_pass'])){
 
+		require_once('popup.php');
 		$user_email = $_POST['User_mail'];
 
 		$new_pass = rand(1000,10000);
-		$update_query = "update `employee` set `password` = $new_pass where `login_id` = '$user_email'";
+		$update_query = "update employee set password = $new_pass where login_id = '$user_email'";
 
 		mysqli_query($conn, $update_query);
 
@@ -80,6 +104,9 @@
 		$headers = "From: aman030904@gmail.com";
 		mail($to_email, $subject, $body, $headers);
 		
+		unset($_SESSION['employee_id']);
+ 		unset($_SESSION['login_id']);
+ 		session_destroy();
 	}
 		
 ?>
