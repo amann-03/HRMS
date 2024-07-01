b4bc0c7002b9ec9628a826f9bc6aa814ebf8650b
Guptesh-Ranjan-Sahoo <128808197+Guptesh-Ranjan-Sahoo@users.noreply.github.com>
2024-07-01 16:26:17 +0530

    Add files via upload

---
+++ project.php
@@ -0,0 +1,48 @@
+<link rel="stylesheet" type="text/css" href="_CSS/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="CSS/styles.css?v=1.1">
+<link href='https://fonts.googleapis.com/css?family=DM Sans' rel='stylesheet'>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<div>
+	<div class="upperbar">
+		<?php require_once('Common/upperbar.php') ?>
+	</div>
+	<div class="left-sidebar">
+		<?php require_once('Common/left-sidebar.php') ?>
+	</div>
+	<div class="mainscreen">
+		<br>
+		<div class="container text-center">
+			
+<div class="row">
+
+<div class="col">
+	<div class="card" id="card2"style="width: 42vw; height: 40vh;">
+		<div class="card-body" >
+			card 5
+		</div>
+	</div>
+</div>
+
+<div class="col"> <div class="card"id ="card1"style="width: 30vw; height: 40vh;">
+	<span class="personalhead">	Project Details </span> 
+	<div class="card-body">
+		card 6
+	</div>
+</div>
+</div>
+	
+</div>
+<div class="row">
+	<div class="col">
+			<div class="card" id="card2"style="width: 73vw; height: 40vh;">
+
+				card 1
+			</div>
+		</div>
+	</div>
+				
+</div>
+</div>
+</div>
