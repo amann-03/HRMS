03f123ce427e454742b5b268b6d306b0d239c234
Guptesh-Ranjan-Sahoo <128808197+Guptesh-Ranjan-Sahoo@users.noreply.github.com>
2024-07-03 14:24:12 +0530

    Add files via upload

--- project.php
+++ project.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" type="text/css" href="_CSS/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="CSS/styles.css?v=1.1">
 <link href='https://fonts.googleapis.com/css?family=DM Sans' rel='stylesheet'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -19,26 +20,62 @@
 
 <div class="col">
 	<div class="card" id="card2"style="width: 42vw; height: 40vh;">
-		<div class="card-body" >
-			card 5
-		</div>
-	</div>
+	<div class="chart_container">	<?php require_once('charts/Project_chart.php') ?></div>
+		
 </div>
+	</div>
 
-<div class="col"> <div class="card"id ="card1"style="width: 30vw; height: 40vh;">
-	<span class="personalhead">	Project Details </span> 
+
+<div class="col"> <div class="card"id ="card1"style="width: 30vw; height: 40vh;	">
+	<span class="personalhead">	Project Details </span>
 	<div class="card-body">
-		card 6
+		<ul class="list-group" id="projectlist">
+			<li class="list-group-item">Project Name :</li>
+			<li class="list-group-item">Client Name :</li>
+			<li class="list-group-item">Domain :</li>
+			<li class="list-group-item">Budget :</li>
+			<li class="list-group-item">Details :</li>
+		</ul>
 	</div>
 </div>
 </div>
 	
 </div>
 <div class="row">
+	<div style="display: block;"><form class="example" action="action_page.php">
+		<input type="text" placeholder="Search.." name="search">
+		<button type="submit"><i class="fa fa-search"></i></button>
+	</form></div>
+</div>
+<div class="row">
 	<div class="col">
-			<div class="card" id="card2"style="width: 73vw; height: 40vh;">
+<div class="listcontainer" style="width: 73vw; height: 37vh;">
+	
 
-				card 1
+	<div class="scrollable">
+		<table class="table table-bordered text-center">
+			<thead>
+				<tr style="background-color: pink;">
+					<th>#</th>
+					<th>Project Name</th>
+					<th>Role</th>
+					<th>Due Date</th>
+					<th>Status</th>
+				</tr>
+				
+			</thead>
+			<tbody>
+			<?php for ($x = 0; $x <= 10; $x++) {?>
+			<tr>	
+				<td>hi</td>
+				<td>helo</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<?php }?>
+			</tbody>
+		</table></div>
 			</div>
 		</div>
 	</div>
