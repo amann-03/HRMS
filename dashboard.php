57e3109dfbb7f9bffef0e48f6df109e744217a2c
Guptesh-Ranjan-Sahoo <128808197+Guptesh-Ranjan-Sahoo@users.noreply.github.com>
2024-07-04 12:32:35 +0530

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" type="text/css" href="_CSS/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="CSS/styles.css?v=1.1">
 <link href='https://fonts.googleapis.com/css?family=DM Sans' rel='stylesheet'>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -17,45 +18,61 @@
 			
 <div class="row">
 
-<div class="col"> <div class="card"id ="card1">
+<a href="Schedule.php"class="col"> <div class="card"id ="card1">
 		<div class="chart_container"><?php require_once('charts/Pie_chart_Dashboard.php') ?></div>
-		
 </div>
-</div>
-	<div class="col"> <div class="card" id="card2">
-		<div class="chart_container"><?php require_once('charts/dash_Bar.php') ?></div>
-		 <div>
+</a>
+	<div class="col" > <div class="card" id="card2" style="display: flex; flex-direction: row; justify-content:space-around;">
+		<div class="bar_container"><?php require_once('charts/dash_Bar.php') ?></div>
+		 <div id="side-list">
          <ul class="list-group" id="dashboardlist">
-			<li class="list-group-item">Project Name :</li>
-			<li class="list-group-item">Client Name :</li>
-			<li class="list-group-item">Domain :</li>
-         
+			<li class="list-group-item">Project Completed :<br><div class="bar-list">9 days</div></li>
+			<li class="list-group-item">Current Project Duration :<br><div class="bar-list">9 days</div></li>
+			<li class="list-group-item">Remaing Duration :<br><div class="bar-list">9 days</div></li>
+         </ul>
 		</div>
+		
+		
          
         
 	</div>
 </div>
 </div>
 <div class="row">
-				<div class="col">
-					<div class="card">
-						<div class="card-body">
-							card 1
-						</div>
-					</div>
-				</div>
-				<div class="col"> <div class="card">
-					<div class="card-body">
-						card 2
-					</div>
-				</div>
-			</div>
-			<div class="col"> <div class="card">
-				<div class="card-body">
-					card 3
-				</div>
+	<div class="col">
+		<div class="card">
+			<div class="card-body">
+				card 1
 			</div>
 		</div>
+	</div>
+	<a href="project.php" class="col"> <div class="card">
+		<div class="card-body" >
+			<dt class="h7">Project Detail</dt>
+			<br>
+			<div style="display:flex; justify-content:space-around;">
+			<dl style="float: left;">
+				<dt>Name</dt>
+				<dd>xyz</dd>
+				<br>
+				<dt>Team Leader</dt>
+				<dd>Abc</dd>
+			</dl>
+			<dl style="float: right;">
+				<dt>#</dt>
+				<dd>A019</dd>
+				<br>
+				<dt>Status</dt>
+				<dd>Ongoing</dd>
+			</dl>
+		</div>
+		</div>
+	</div>
+</a>
+<div class="col"> <div class="card">
+	card
+</div>
+</div>
 </div>
 </div>
 </div>
