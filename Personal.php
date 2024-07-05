7408c6c585ad9a5ac39a469fc3cf2c4e8f6ec277
Guptesh-Ranjan-Sahoo <128808197+Guptesh-Ranjan-Sahoo@users.noreply.github.com>
2024-07-05 11:27:17 +0530

    Add files via upload

--- Personal.php
+++ Personal.php
@@ -1,11 +1,9 @@
-<?php session_start();
-require_once('connection.php');
-
-if (isset($_SESSION['employee_id'])) { ?>
-
 <link rel="stylesheet" type="text/css" href="_CSS/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="CSS/styles.css?v=1.1">
 <link href='https://fonts.googleapis.com/css?family=DM Sans' rel='stylesheet'>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.6/dist/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 <div>
 	<div class="upperbar">
 <?php require_once('Common/upperbar.php') ?>
@@ -19,49 +17,40 @@ if (isset($_SESSION['employee_id'])) { ?>
 <div class="container text-center">
 <div class="row">
 <div class="col"><div class="col">
- <div class="card"id ="card1"  style="height: 80vh;">
+ <div class="card"id ="card1"  style="height: 83vh;">
  			<img class="card-img-top" src="Photo/image 5.png" alt="Card image cap">
 	<div class="card-body">
-
-
-		<?php 
-			$qer = "SELECT name, joining_date from employee join emp_depart on employee.department_id = emp_depart.department_id where employee.employee_id = ".$_SESSION['employee_id'];
-			$res = mysqli_query($conn, $qer);
-			$ans = mysqli_fetch_object($res);
-		 ?>
-
-				<p class="fw-bolder">Name<br><?php echo $ans->name; ?></p>
-				<span class="title">at work for <?php ?></span>
+				<h4 class="fw-bolder">Name<?php$name?></h4>
+				<span class="title">at work for : <?php$join-$current?></span>
 				<br>
 				<h3>Team Members<h3>
 				<ul class="list-group" id="projectlist">
 					<br>
-
-
 			<li class="list-group-item">Person1</li>
 			<li class="list-group-item">Person2</li>
 			<li class="list-group-item">Person3</li>
 		</ul>
 	</div>
 </div></div></div>
-	
-<div class="col"><div class="row">
+
+<div class="col">
+
+	<div class="row">
 	 <div class="card" id="card2" style="height: 49vh;">
-	 	<span class="personalhead">	Personal Details </span>
-	 	<div class="card-body">
-	 		
+	 	<span class="personalhead" id="personalhead2">	Personal Details 
+	 	<button type="button" style="position: absolute;
+    right: 2vw;
+    height: 4vh;
+    padding-top: 0;"class="btn btn-dark" data-toggle="modal" data-target="#Modal2">Edit</button>
+
+	 	</span>
+
+	 	<div class="card-body">	
 
 				<?php
-	
-				$statement = "SELECT name, work_status, department_id, employee_id, login_id, dob, gender, phone_number, address FROM employee where employee_id = ".$_SESSION['employee_id'];
+				$rows=array("Name"=>"#","ID"=>"#","Date of Birth"=>"#","Gender"=>"#","Email"=>"#","Phone no."=>"#","address"=>"#");
+				?>
 
-				$res = mysqli_query($conn, $statement);
-				$row = mysqli_fetch_object($res);
-
-
-				$rows=array("Name"=>$row->name,"ID"=>$row->employee_id,"Date of Birth"=>date('d-m-Y', strtotime($row->dob)),"Gender"=>$row->gender,"Email"=>$row->login_id,"Phone no."=>$row->phone_number,"Address"=>$row->address);
-
-?>
 					<table style="width: 100%;">
 				<?php
 				foreach ($rows as $key => $value): ?>
@@ -75,25 +64,14 @@ if (isset($_SESSION['employee_id'])) { ?>
 		</div>
 	</div></div>
 	<div class="row">
-	 <div class="card" id="card2" style="height: 30vh;">
-		<span class="personalhead"> Company Details </span>
+	 <div class="card" id="card2" style="height: 32vh;">
+		<span class="personalhead" id="personalhead3"> Company Details </span>
 	 	<div class="card-body">
 	 		
 
 
 				<?php
-
-				$query1 = "SELECT department_name from department where department_id = $row->department_id";
-				$query2 =  "select designation, joining_date from emp_depart where employee_id = ".$_SESSION['employee_id'];
-
-				$res1 = mysqli_query($conn, $query1);
-				$row1 = mysqli_fetch_object($res1);
-
-				$res2 = mysqli_query($conn, $query2);
-				$row2 = mysqli_fetch_object($res2);
-
-				$rows=array("Department"=>$row1->department_name,"Postion"=>$row2->designation,"Status"=>$row->work_status,"Joining Date"=>date('d-m-Y', strtotime($row2->joining_date)));
-
+				$rows=array("Department"=>"#","postion"=>"#","status"=>"#","project"=>"#");
 				?>
 
 					<table style="width: 100%;">
@@ -113,9 +91,4 @@ if (isset($_SESSION['employee_id'])) { ?>
 </div>
 </div>
 </div>
-<?php } 
-else{
-	header('location:index.php');
-}
-
-?>
+<?php require_once('popup2.php')?>
