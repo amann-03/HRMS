648ea4c38d97f489529125d6418d341fc3ed69f9
Guptesh-Ranjan-Sahoo <128808197+Guptesh-Ranjan-Sahoo@users.noreply.github.com>
2024-07-10 14:51:10 +0530

    Add files via upload

--- attendance.php
+++ attendance.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" type="text/css" href="_CSS/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="CSS/styles.css?v=1.1">
+<link rel="stylesheet" type="text/css" href="calendar/calendar.css?v=1.1">
 <link href='https://fonts.googleapis.com/css?family=DM Sans' rel='stylesheet'>
 <div>
 	<div class="upperbar">
@@ -20,23 +21,43 @@
 <div class="container text-center">
 <div class="row">
 <div class="col"><div class="col">
- <div class="card"id ="card1" >
-	<div class="card-body">
-		monthly attendance Stat
-	</div>
+ <div class="listcontainer">
+ 	<div class="attmark">Mark Your Attendance</div>
+ 	<?php
+		if (isset($_POST['mark'])){
+			 echo "<h3>Attendance Marked</h3>";?>
+			 <br>
+			<h3>Status:<h3>
+			<div style="color:"> <?php echo "Pending" ?> </div>
+	<?php	}
+		else{
+		?>
+		<form action="" method="post"><button name="mark" type="submit" class="btn btn-info" id="mark">Mark</button></form>
+		<?php }?>
+
+		
 </div>
 <div class="card"id ="card1" >
 	<div class="card-body">
-		total leave Stat
+		<form action="" method="post">
+		<div>
+            <label><h3 style="font-size: 3.5vh; padding-bottom: 1vh; font-weight:700">Remark</h3> </label>
+          </div>
+          <div>
+          	<textarea style="background-color:#49505714;"  class="input_div" name="Rem" placeholder="If Late-Punch-in is shown please give the remark" rows='4'></textarea>
+          </div>
+          <br>
+        <div>
+          <button name="mark" type="submit" class="btn btn-info" id="mark">Submit</button>
+        </div>
+        </form>
 	</div>
 </div>
 </div></div>
 	
 <div class="col"><div class="row">
 	 <div class="card" id="card2" style="height:79vh">
-		<div class="card-body">
-			meeting stat
-		</div>
+			<?php require_once("calendar/calendar_show.php")?>
 	</div></div>
 </div>
 </div>
