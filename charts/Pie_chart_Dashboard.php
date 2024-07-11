c96b328b57a719c8f34f53b0864da03f5b27770d
Guptesh-Ranjan-Sahoo <128808197+Guptesh-Ranjan-Sahoo@users.noreply.github.com>
2024-07-11 18:05:55 +0530

    Add files via upload

--- charts/Pie_chart_Dashboard.php
+++ charts/Pie_chart_Dashboard.php
@@ -22,49 +22,23 @@ var barColors = [
     borderColor: "rgba(255, 255, 255, 1)",
     borderWidth: 2,
     borderRadius: 10,
+     hoverBackgroundColor: "rgba(125,9,102,0.2)",
+    hoverBorderColor: "rgba(25,44,102,1)",
+
       backgroundColor: barColors,
       data: yValues
 
 
     }]
   },
-  options:  {
+options:  {
     responsive:true,
-  events: false,
-  animation: {
-    duration: 500,
-    easing: "easeOutQuart",
-    onComplete: function () {
-      var ctx = this.chart.ctx;
-      ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontFamily, 'normal', Chart.defaults.global.defaultFontFamily);
-
-        ctx.font =   `18px Verdana`;
-      ctx.textAlign = 'center';
-      ctx.textBaseline = 'middle';
-
-      this.data.datasets.forEach(function (dataset) {
-
-        for (var i = 0; i < dataset.data.length; i++) {
-          var model = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model,
-              total = dataset._meta[Object.keys(dataset._meta)[0]].total,
-              mid_radius = model.innerRadius + (model.outerRadius - model.innerRadius)/2,
-              start_angle = model.startAngle,
-              end_angle = model.endAngle,
-              mid_angle = start_angle + (end_angle - start_angle)/2;
-
-          var x = mid_radius * Math.cos(mid_angle);
-          var y = mid_radius * Math.sin(mid_angle);
-
-          ctx.fillStyle = '#fff';
-          if (i == 3){ // Darker text color for lighter background
-            ctx.fillStyle = '#444';
-          }
-          var percent = String(Math.round(dataset.data[i]/total*100)) + "%";
-          ctx.fillText(dataset.data[i], model.x + x, model.y + y);
-        }
-      });               
-    }
-  }
+   legend:{
+      position:'bottom',
+      align:'center'},
+    tooltips:{
+        bodyFontSize:16
+      }
 }
 });
 </script>
