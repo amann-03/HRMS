0913af9740a40e3036c28ed02898d4a38e87243d
Aman Patel <122123520+amann-03@users.noreply.github.com>
2024-07-23 16:13:44 +0530

    Delete Dash_el directory

--- Dash_el/clock.php
+++
@@ -1,5 +0,0 @@
-<?php date_default_timezone_set("Asia/Kolkata");
-	?>
-<div style="padding: 0.5vw;font-size: 4vh; font-weight: 700;"><?php echo date("h:i:s A")?></div>
-
-<div style="padding: 0.5vw;font-size: 3vh;"><?php echo date("l jS  F Y")?></div>
