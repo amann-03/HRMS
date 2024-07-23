261bdedab882b4a6657000327f8dd1ae890a0d7c
Aman Patel <122123520+amann-03@users.noreply.github.com>
2024-07-23 16:13:54 +0530

    Delete HR directory

--- HR/header.php
+++
@@ -1,7 +0,0 @@
-     <img src="indo.png" id="logo" alt="logo">
-         <a href id="aboutus">About Us</a>
-        <select id="Settings"> 
-            <option value="Home">Home</option>
-            <option value="View Profile">View Profile</option>
-            <option value="Configure">Configure</option>
-        </select>
